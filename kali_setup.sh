a294af0a248696804c1672c4e3f5775257aa9a80
Scott <scott@SMB.local>
2016-03-29 20:24:12 -0400

    Update ST3 URL

--- kali_setup.sh
+++ kali_setup.sh
@@ -15,7 +15,7 @@ cmd_exe () {
 ##########################
 ##      Variables       ##
 ##########################
-SUBLIME_URL="http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb"
+SUBLIME_URL="https://download.sublimetext.com/sublime-text_build-3103_i386.deb" ## ST3 Build 3103
 TIMEZONE="America/New_York"
 
 ##########################
