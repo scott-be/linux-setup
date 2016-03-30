7658eab692195ac6d3d323fa41e24fa3050d53a0
Scott <scott@SMB.local>
2016-03-29 20:24:34 -0400

    Remove Chromium install

--- kali_setup.sh
+++ kali_setup.sh
@@ -51,9 +51,6 @@ cmd_exe "apt-get install -y openssh-server"
 printf '[+] Installing Sublime Text 3...'
 cmd_exe "wget -O $HOME/sublime_text_3.deb $SUBLIME_URL && sudo dpkg -i $HOME/sublime_text_3.deb && rm $HOME/sublime_text_3.deb"
 
-printf '[+] Installing Chromium...'
-cmd_exe "apt-get install -y chromium"
-
 ##########################
 ##      Tweaks...       ##
 ##########################
