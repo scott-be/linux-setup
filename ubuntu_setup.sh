3d4fdd5a4da9a69870bc6588c0305e9d173c2b60
Scott <scott@SMB.local>
2016-11-08 17:07:52 -0500

    color output

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17,55 +17,55 @@ TIMEZONE="America/New_York"
 ##########################
 ## First things first...##
 ##########################
-printf '[+] Updating Ubuntu...'
+printf '  \e[1;34m[+]\e[0m Updating Ubuntu...'
 cmd_exe "sudo apt-get update && sudo apt-get upgrade -y"
 
 ##########################
 ##      App Installs... ##
 ##########################
-printf '[+] Installing git...'
+printf '  \e[1;34m[+]\e[0m Installing git...'
 cmd_exe "sudo apt-get install -y git"
 
-printf '[+] Installing SSH server...'
+printf '  \e[1;34m[+]\e[0m Installing SSH server...'
 cmd_exe "sudo apt-get install -y openssh-server"
 
-printf '[+] Installing Sublime Text 3...'
+printf '  \e[1;34m[+]\e[0m Installing Sublime Text 3...'
 cmd_exe "wget -O $HOME/sublime_text_3.deb $SUBLIME_URL && sudo dpkg -i $HOME/sublime_text_3.deb && rm $HOME/sublime_text_3.deb"
 
-printf '[+] Installing Chromium...'
+printf '  \e[1;34m[+]\e[0m Installing Chromium...'
 cmd_exe "sudo apt-get install -y chromium"
 
 ##########################
 ##       Tweeks...      ##
 ##########################
-printf '[+] Disabling screen lock...'
+printf '  \e[1;34m[+]\e[0m Disabling screen lock...'
 cmd_exe "gsettings set org.gnome.desktop.lockdown disable-lock-screen true"
 
-printf '[+] Disabling screensaver...'
+printf '  \e[1;34m[+]\e[0m Disabling screensaver...'
 cmd_exe "gsettings set org.gnome.desktop.session idle-delay 0"
 
-printf '[+] Show date in menu bar...'
+printf '  \e[1;34m[+]\e[0m Show date in menu bar...'
 cmd_exe "gsettings set org.gnome.desktop.interface clock-show-date true"
 
-printf '[+] Enable desktop icons...'
+printf '  \e[1;34m[+]\e[0m Enable desktop icons...'
 cmd_exe "gsettings set org.gnome.desktop.background show-desktop-icons true"
 
-printf '[+] Show home icon on desktop...'
+printf '  \e[1;34m[+]\e[0m Show home icon on desktop...'
 cmd_exe "gsettings set org.gnome.nautilus.desktop home-icon-visible true"
 
-printf '[+] Show trash icon on desktop...'
+printf '  \e[1;34m[+]\e[0m Show trash icon on desktop...'
 cmd_exe "gsettings set org.gnome.nautilus.desktop trash-icon-visible true"
 
-printf '[+] Add minimize, maximize, close buttons to windows...'
+printf '  \e[1;34m[+]\e[0m Add minimize, maximize, close buttons to windows...'
 cmd_exe "gsettings set  org.gnome.shell.overrides button-layout ':minimize,maximize,close'"
 
-printf '[+] 12 hour time...'
+printf '  \e[1;34m[+]\e[0m 12 hour time...'
 cmd_exe "gsettings set org.gnome.desktop.interface clock-format '12h'"
 
-printf '[+] Changing time zone...'
+printf '  \e[1;34m[+]\e[0m Changing time zone...'
 cmd_exe "echo $TIMEZONE > /etc/timezone && ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime && dpkg-reconfigure -f noninteractive tzdata"
 
-printf '[+] Disabling super key...'
+printf '  \e[1;34m[+]\e[0m Disabling super key...'
 cmd_exe "gsettings set org.gnome.mutter overlay-key ''"
 
 ##########################
@@ -101,10 +101,10 @@ cmd_exe "dconf load /org/gnome/terminal/legacy/profiles:/ < resources/monokai-so
 ##########################
 ##    Setup configs...  ##
 ##########################
-printf '[+] Configuring Sublime Text...'
+printf '  \e[1;34m[+]\e[0m Configuring Sublime Text...'
 cmd_exe "rm -rf $HOME/.config/sublime-text-3/Packages/User/ && git clone https://github.com/scott-be/Sublime-Text-3-Settings.git $HOME/.config/sublime-text-3/Packages/User/"
 
-printf '[+] Setting up dotfiles (mc, bash, tmux, vim, etc.)...'
+printf '  \e[1;34m[+]\e[0m Setting up dotfiles (mc, bash, tmux, vim, etc.)...'
 cmd_exe "cd $HOME && git clone https://github.com/scott-be/dotfiles.git && bash dotfiles/make.sh --linux && cd -"
 
 
