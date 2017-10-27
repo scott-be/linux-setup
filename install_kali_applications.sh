0306325826b5a49b9947bcc4d5c38de683830a7f
Scott <sb1975@gmail.com>
2017-10-27 00:07:22 -0400

    install go

--- install_kali_applications.sh
+++ install_kali_applications.sh
@@ -26,6 +26,9 @@ cmd_exe "wget -O /var/tmp/shareenum_2.0_amd64.deb https://github.com/CroweCybers
 printf '  \e[1;34m[+]\e[0m Install mana-toolkit...'
 cmd_exe "apt-get install mana-toolkit -y && ln -s /usr/share/mana-toolkit/ /sbtools/mana-toolkit"
 
+printf '  \e[1;34m[+]\e[0m Install go...'
+cmd_exe "apt-get install golang -y"
+
 printf '\e[1;4;94mCloning repos\e[0m\n'
 mkdir -p /sbtools
 cd /sbtools
