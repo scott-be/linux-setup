7b2e2f14a7fbf2f8cb2b49c7bea6887f280af43e
Scott <sb1975@gmail.com>
2018-11-10 21:36:56 -0500

    apt-get -> apt

--- install-applications-kali.sh
+++ install-applications-kali.sh
@@ -18,19 +18,19 @@ cmd_exe () {
 ##########################
 printf '\e[1;4;94mApp Installs\e[0m\n'
 printf '  \e[1;34m[+]\e[0m Installing cifs-utils...'
-cmd_exe "apt-get install cifs-utils -y"
+cmd_exe "apt install cifs-utils -y"
 
 printf '  \e[1;34m[+]\e[0m Installing shareenum...'
 cmd_exe "wget -O /var/tmp/shareenum_2.0_amd64.deb https://github.com/CroweCybersecurity/shareenum/releases/download/2.0/shareenum_2.0_amd64.deb && dpkg -i /var/tmp/shareenum_2.0_amd64.deb && rm /var/tmp/shareenum_2.0_amd64.deb"
 
 printf '  \e[1;34m[+]\e[0m Install mana-toolkit...'
-cmd_exe "apt-get install mana-toolkit -y && ln -s /usr/share/mana-toolkit/ /sbtools/mana-toolkit"
+cmd_exe "apt install mana-toolkit -y && ln -s /usr/share/mana-toolkit/ /sbtools/mana-toolkit"
 
 printf '  \e[1;34m[+]\e[0m Install go...'
-cmd_exe "apt-get install golang -y"
+cmd_exe "apt install golang -y"
 
 printf '  \e[1;34m[+]\e[0m Install bettercap...'
-cmd_exe "apt-get install bettercap -y"
+cmd_exe "apt install bettercap -y"
 
 
 printf '\e[1;4;94mCloning repos\e[0m\n'
@@ -45,7 +45,6 @@ done < $DIR/resources/github-repos-kali.txt
 cd $DIR
 
 # TODO
-# [ ] Loop github repos file (resources/github-repos-kali.txt)
 # [ ] Install Impacket - cd /sbtools/impacket/ && python setup.py install
 ## Install (requires interaction)
 # [ ] Install Empire - /sbtools/Empire/setup/install.sh
